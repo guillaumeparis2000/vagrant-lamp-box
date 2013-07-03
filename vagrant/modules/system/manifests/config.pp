c315c59f9a87af77b38e01abe62559218ca62269
Guillaume PARIS <guillaume.paris.work@gmail.com>
2013-07-03 13:48:54 +0200

    Remove modules

--- vagrant/modules/system/manifests/config.pp
+++
@@ -1,8 +0,0 @@
-define system::config ($source, $ensure = 'present') {
-	file {
-        "System_${name}":
-        	name => "/home/vagrant/${name}",
-            ensure => $ensure,
-            source => $source,
-    }
-}
