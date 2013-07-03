930aa1167307ab739a22f20c1692970755b4e421
Guillaume PARIS <guillaume.paris.work@gmail.com>
2013-07-03 13:44:33 +0200

    Clean mymodules/system code and add some documentation

--- vagrant/mymodules/system/manifests/config.pp
+++ vagrant/mymodules/system/manifests/config.pp
@@ -1,8 +1,28 @@
+# == Define: package
+#
+# This class allow to create system configuration files
+#
+# === Parameters
+#
+# [*name*]
+#   name of the configuration file
+#
+# [*source*]
+#   Source file to create the configuration file
+#
+# === Examples
+#
+# Provide some examples on how to use this type:
+#   system::config { "bashrc":
+#       name   => ".bashrc",
+#      source => "/vagrant/files/system/bashrc",
+#  }
+#
 define system::config ($source, $ensure = 'present') {
-	file {
+  file {
         "System_${name}":
-        	name => "/home/vagrant/${name}",
-            ensure => $ensure,
-            source => $source,
+        ensure  => $ensure,
+        name    => "/home/vagrant/${name}",
+        source  => $source,
     }
 }
